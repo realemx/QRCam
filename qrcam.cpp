25c88a5de794142e6ebacf085c785d81cc822c6e
realemx <pascal.liedkte@me.com>
2019-09-12 20:54:25 +0200

    removed potentially dangerous using-directives

--- qrcam.cpp
+++ qrcam.cpp
@@ -3,55 +3,51 @@
 #include <opencv2/core/types.hpp>
 #include "qrcam.h"
 
-using namespace cv;
-using namespace std;
 
-
-int main(int argc, char* argv[] )
-{
-	VideoCapture stream1(0);
+int main(int argc, char* argv[] ) {
+	cv::VideoCapture stream1(0);
 
 	if (!stream1.isOpened()) {
-		cerr << "\t\033[1;31mcannot open camera!\033[0;30m" << endl;
+		std::cerr << "\t\033[1;31mcannot open camera!\033[0;30m" << std::endl;
 		return 1;
 	}
 
-	Mat inFrame, outFrame, points;
-	QRCodeDetector qrDecoder = QRCodeDetector();
+	cv::Mat inFrame, outFrame, points;
+	cv::QRCodeDetector qrDecoder = cv::QRCodeDetector();
 
-	namedWindow("Camera", WINDOW_NORMAL);
+	namedWindow("Camera", cv::WINDOW_NORMAL);
 
-	cout << endl;
+	std::cout << std::endl;
 
 	while (true) {
 		stream1.read(inFrame);
 
 		if(qrDecoder.detect(inFrame, points)) {
-			cout << "\r" << qrDecoder.decode(inFrame, points);
+			std::cout << "\r" << qrDecoder.decode(inFrame, points);
 			// making sure the line is clean by printing a lot of whitespaces
-			cout << "                                                                                                 ";
+			std::cout << "                                                                                                 ";
 			drawMatch(inFrame, points);
 		}
 
 		flip(inFrame, outFrame, +1);
 		imshow("Camera", outFrame);
 
-		if (waitKey(30) >= 0) {
+		if (cv::waitKey(30) >= 0) {
 			break;
 		}
 	}
 
-	cout << endl;
+	std::cout << std::endl;
 	return 0;
 }
 
-void drawMatch(Mat image, Mat points) {
+void drawMatch(cv::Mat image, cv::Mat points) {
 	int n = points.rows;
 	for(int i = 0 ; i < n ; i++)
 	{
 		line(image,
-			 Point(points.at<float>(i, 0), points.at<float>(i, 1)),
-			 Point(points.at<float>((i + 1) % n, 0), points.at<float>((i + 1) % n, 1)),
-			 Scalar(0, 0, 255), 3);
+			 cv::Point(points.at<float>(i, 0), points.at<float>(i, 1)),
+			 cv::Point(points.at<float>((i + 1) % n, 0), points.at<float>((i + 1) % n, 1)),
+			 cv::Scalar(0, 0, 255), 3);
 	}
 }
