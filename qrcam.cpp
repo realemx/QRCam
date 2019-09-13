9f7de60b5bd9aab727d162f40835eadf747e93db
realemx <pascal.liedkte@me.com>
2019-09-13 10:56:17 +0200

    increased match probability

--- qrcam.cpp
+++ qrcam.cpp
@@ -13,9 +13,11 @@ int main(int argc, char* argv[] ) {
 	int videoDevice = 0;
 	bool onlyReadOne = false;
 	std::string winName = "Camera";
+	float alpha = 0.5;
+	int beta = 70;
 
 
-	while ((option = getopt(argc, argv, "ohv:w:")) != EOF) {
+	while ((option = getopt(argc, argv, "ohv:w:a:b:")) != EOF) {
 		switch(option) {
 			case 'v':
 				videoDevice = std::stoi(optarg);
@@ -28,6 +30,12 @@ int main(int argc, char* argv[] ) {
 				break;
 			case 'w':
 				winName = optarg;
+				break;
+			case 'a':
+				alpha = std::stof(optarg);
+				break;
+			case 'b':
+				beta = std::stoi(optarg);
 				break;
 			default:
 				return 1;
@@ -56,18 +64,26 @@ int main(int argc, char* argv[] ) {
 	while (true) {
 		stream1.read(inFrame);
 
+		// Copy frame before greyscale conversion
+		outFrame = inFrame.clone();
+
+		cv::cvtColor(inFrame, inFrame, cv::COLOR_BGR2GRAY);
+		inFrame.convertTo(inFrame, -1, alpha, beta);
+
 		if(qrDecoder.detect(inFrame, points)) {
 			qrString = qrDecoder.decode(inFrame, points);
 			std::cout << "\r" << qrString;
 			// making sure the line is clean by printing a lot of whitespaces
 			std::cout << "                                                                                            ";
-			drawMatch(inFrame, points);
+			//Draw outlines on the colored image
+			drawMatch(outFrame, points);
 			if (!qrString.empty()) {
 				readQR = true;
 			}
 		}
 
-		flip(inFrame, outFrame, +1);
+		//Flip the image for a mirror like video
+		flip(outFrame, outFrame, +1);
 		imshow(winName, outFrame);
 
 		if (cv::waitKey(30) >= 0 || (readQR && onlyReadOne)) {
@@ -94,9 +110,11 @@ void printhelp() {
 	std::cout << "Usage: qrcam [OPTIONS]" << std::endl;
 	std::cout << "" << std::endl;
 	std::cout << "Options:" << std::endl;
-	std::cout << "\t-o:\t\tread one code and exit the program automatically" << std::endl;
+	std::cout << "\t-o:\t\tRead one code and exit the program automatically" << std::endl;
 	std::cout << "\t-h:\t\tPrint this help site" << std::endl;
-	std::cout << "\t-v ID\t\tSelect video devide with the id ID (Default ID = 0)" << std::endl;
-	std::cout << "\t-w NAME\t\tSets the Window Name to NAME (Default = Camera)" << std::endl;
+	std::cout << "\t-v id\t\tSelect video devide with the id  (Default: 0)" << std::endl;
+	std::cout << "\t-w name\t\tSets the Window Name to NAME (Default: Camera)" << std::endl;
+	std::cout << "\t-a value\t\tSets a custom alpha value (Default: 0.5)" << std::endl;
+	std::cout << "\t-b value\t\tSets a custom beta value (Default: 70)" << std::endl;
 	std::cout << "" << std::endl;
 }
